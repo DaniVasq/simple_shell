d0da7eadce3708568894c50fdc461923563d6b38
DaniVasq <danivasq50@gmail.com>
2019-04-08 19:05:42 +0000

    Uploading to shared repo to work on together

--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-/** 
+/**
  * Description: Hi, Laura, this code pretend help to work of the team, allow show the each features, and internal process about the program the shell to get ahead the program, by Fesus Rocuts
  */
 /** the minimal headers */
@@ -14,43 +14,45 @@
 
 /** fn prototypes*/
 void prompt(void);
-void get_data_line(void);
 int listenread(char *);
-void get_simple_args(void);
+void get_simple_args(char *args);
 
 /** fn entry to launch shell program*/
-int main(void)
-{
-	/** simply and power fn that listen of and process the features the program*/
-	get_data_line();
-	return (0);
-}
 /** this fn allow get data of the client terminal to after process, when it's pass the fn is call newly*/
-void get_data_line(void)
+int main (int argc, char *argv[] __attribute((unused)))
 {
 	/** pointer used to save data input of the terminal client */
 	char *line;
+	char delim[] = " ";
+	char *args;
+	/**char *argsv[1] = {""};*/
 
-	line = malloc(sizeof(char) * SIZEBUFFER + 1);
+        /** launch prompt */
+	prompt();
+
+	if (argc == 1)
+	{
+        line = malloc(sizeof(char) * SIZEBUFFER + 1);
 	if (!line)
 		exit(100);
 	line[SIZEBUFFER] = '\0';
 
-	/** launch prompt */
-	prompt();
-
 	/** init the read and write data input of client, it's recursive fn*/
 	if(listenread(line))
 		printf("get data line >> %s\n", line);
+	args = strtok(line, delim);
+	printf("%s\n", args);
+	}
 
 	/** parse the pointer to exec the command*/
-	get_simple_args();
+	/**get_simple_args(args);*/
 
 	/** the memory is unblock*/
 	free(line);
 
 	/** call himself, fn recursive */
-	get_data_line();
+	/*main(1, NULL);*/
+	return (1);
 }
 /** Description: this fn allow print symbol or mesage before that user write*/
 void prompt(void)
@@ -61,31 +63,20 @@ void prompt(void)
 	putchar(32);
 }
 /** Description; this fn allow get char the client terminal and call me fn until char is '\n'*/
-int listenread(char *line)
+int listenread(char *buffer)
 {
-	/** the var that receive a char and save into pointer*/
-	int c;
+	size_t bufsize = SIZEBUFFER;
+	size_t c;
 
-	/** the client write into terminal */
-	c = getchar();
-	/** statement that exit when it reaches the end file */
-	if(c == EOF)
-		exit(101);
-	/** when find end line the same type key enter or return */
-	else if(c == '\n')
-	{
-		*line = '\0';
-		return 1;
-	}
-	/** save the char into var*/
-	*line = c;
-	/** the pointer is moved to the next position and call himself*/
-	listenread(line + 1);
+	c = getline(&buffer, &bufsize, stdin);
+	if (!c)
+		return (-1);
+	return (1);
 }
-/** 
+/**
 * Description: this fn allow split the string when find the space char
 */
-void get_simple_args(void)
+void get_simple_args(char *args __attribute__((unused)))
 {
-	
+
 }
