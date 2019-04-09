75e432bd393753247a373aa9e0a443e2b14df6c7
Fesus Rocuts <fesusrocuts@gmail.com>
2019-04-09 02:03:25 +0000

    remove fix, the delimiter is 'SPACE+LINE_BREAK' it's work!

--- shell.c
+++ shell.c
@@ -10,12 +10,15 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 #define SIZEBUFFER 1024
 
 /** fn prototypes*/
 void prompt(void);
 int listenread(char *);
-void get_simple_args(char *args);
+void get_simple_args(int, char **, char *);
+void get_simple_args2(char *);
+char** str_split(char* a_str, const char a_delim);
 
 /** fn entry to launch shell program*/
 /** this fn allow get data of the client terminal to after process, when it's pass the fn is call newly*/
@@ -23,35 +26,33 @@ int main (int argc, char *argv[] __attribute((unused)))
 {
 	/** pointer used to save data input of the terminal client */
 	char *line;
-	char delim[] = " ";
-	char *args;
-	/**char *argsv[1] = {""};*/
+	long int i = 0;
+	long int i2 = 0;
+	char *argv2[] = {__FILE__, NULL};
 
         /** launch prompt */
 	prompt();
 
 	if (argc == 1)
 	{
-        line = malloc(sizeof(char) * SIZEBUFFER + 1);
-	if (!line)
-		exit(100);
-	line[SIZEBUFFER] = '\0';
+        	line = malloc(sizeof(char) * SIZEBUFFER + 2);
+		if (!line)
+			exit(100);
+		line[SIZEBUFFER] = '\0';
 
-	/** init the read and write data input of client, it's recursive fn*/
-	if(listenread(line))
-		printf("get data line >> %s\n", line);
-	args = strtok(line, delim);
-	printf("%s\n", args);
+		/** init the read and write data input of client, it's recursive fn*/
+		if(listenread(line))
+			printf("get data line >> %s\n", line);
 	}
 
 	/** parse the pointer to exec the command*/
-	/**get_simple_args(args);*/
+	get_simple_args(argc, argv, line);
 
 	/** the memory is unblock*/
 	free(line);
 
 	/** call himself, fn recursive */
-	/*main(1, NULL);*/
+	/*main(1, argv2);*/
 	return (1);
 }
 /** Description: this fn allow print symbol or mesage before that user write*/
@@ -73,10 +74,53 @@ int listenread(char *buffer)
 		return (-1);
 	return (1);
 }
+
 /**
 * Description: this fn allow split the string when find the space char
 */
-void get_simple_args(char *args __attribute__((unused)))
+void get_simple_args(int argc, char **argv __attribute__((unused)), char *args __attribute__((unused)))
 {
+	char delim[] = " \n";
+	pid_t pid;
+	char *envp[] = {"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin",NULL};
+	char *command;
+	int status;
+	int i = 0;
+	int existsdelim = 0;
 
+	if (argc == 1)
+	{
+		while (args[i] != '\0')
+			i++;
+		command = (char *)strtok(args, delim);
+
+		if (i > 0)
+			get_simple_args2((char *)command);
+	}
+	else if(argc > 1)
+		get_simple_args2(argv[1]);
+}
+
+void get_simple_args2(char *command)
+{
+	pid_t pid;
+	char *envp[] = {"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin",NULL};
+	int status;
+	char *_argv[] = {command, NULL};
+
+	pid = fork();
+	if (pid == 0)
+		if (execve(command, _argv, envp) == -1)
+		{
+			printf("Error execve\n");
+			exit(103);
+		}
+	else if (pid < 0)
+	{
+		printf("Error pid < 0\n");
+		exit(102);
+	}
+		do{
+			waitpid(pid, &status, 0);
+		}while(!WIFEXITED(status) && !WIFSIGNALED(status));
 }
