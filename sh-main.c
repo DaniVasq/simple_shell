df3ac1a0bb83a4ae089ad6855cf0d8baa75b47d4
DaniVasq <danivasq50@gmail.com>
2019-04-10 04:58:44 +0000

    [feature] separating main shell code into three files for organization

---
+++ sh-main.c
@@ -0,0 +1,41 @@
+3#include "shell.h"
+
+/**
+  * main - entry
+  *
+  * Return: 1.
+  */
+int main(int argc, char *argv[] __attribute((unused)))
+{
+/** pointer used to save data input of the terminal client */
+	char *line;
+	char *argv2[] = {__FILE__, NULL};
+	int _isatty;
+
+	_isatty = isatty(0);
+	if (argc == 1)
+{
+/** launch prompt */
+	if (_isatty != 0)
+		prompt();
+	line = malloc(sizeof(char) * SIZEBUFFER + 2);
+	if (!line)
+		exit(100);
+	line[SIZEBUFFER] = '\0';
+
+/** init the read and write data input of client, it's recursive fn*/
+	if (listenread(line))
+		;
+/*printf("get data line >> %s\n", line);*/
+}
+
+/** parse the pointer to exec the command*/
+	get_simple_args(argc, argv, line);
+
+/**fseek(stdin, 0, SEEK_END);*/
+	fflush(stdin);
+/** call himself, fn recursive */
+	if (_isatty != 0)
+		main(1, argv2);
+	return (1);
+}
