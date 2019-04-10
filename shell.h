302c615e4bd49ee183149a45734defca44916a71
DaniVasq <danivasq50@gmail.com>
2019-04-10 04:52:51 +0000

    [organization]Separated the code into different files like header.h, main.c, and shell.c

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#define SIZEBUFFER 1024
+
+void prompt(void);
+int listenread(char *);
+void get_simple_args(int, char **, char *);
+void pid_launch(char *);
+void _error(void);
+
+#enfif
