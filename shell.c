ebdfb4210d8a3d37f7426d24e93665b8d5e24ef5
DaniVasq <danivasq50@gmail.com>
2019-04-10 15:52:42 +0000

    [fix] betty documentation style, added descriptions

--- shell.c
+++ shell.c
@@ -1,62 +1,8 @@
 /**
- * Description: Hi, Laura, this code pretend help to work of the team, allow show the each features, and internal process about the program the shell to get ahead the program, by Fesus Rocuts
+ * prompt - function for the print symbol before the user types
+ *
+ * Return: nothing.
  */
-/** the minimal headers */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-#include <assert.h>
-#define SIZEBUFFER 1024
-
-/** fn prototypes*/
-void prompt(void);
-int listenread(char *);
-void get_simple_args(int, char **, char *);
-void pid_launch(char *);
-void _error(void);
-
-/** fn entry to launch shell program*/
-/** this fn allow get data of the client terminal to after process, when it's pass the fn is call newly*/
-int main(int argc, char *argv[] __attribute((unused)))
-{
-	/** pointer used to save data input of the terminal client */
-	char *line;
-	char *argv2[] = {__FILE__, NULL};
-	int _isatty;
-
-	_isatty = isatty(0);
-	if (argc == 1)
-	{
-		/** launch prompt */
-		if (_isatty != 0)
-			prompt();
-		line = malloc(sizeof(char) * SIZEBUFFER + 2);
-		if (!line)
-			exit(100);
-		line[SIZEBUFFER] = '\0';
-
-		/** init the read and write data input of client, it's recursive fn*/
-		if (listenread(line))
-			;
-			/*printf("get data line >> %s\n", line);*/
-	}
-
-	/** parse the pointer to exec the command*/
-	get_simple_args(argc, argv, line);
-
-	/**fseek(stdin, 0, SEEK_END);*/
-	fflush(stdin);
-	/** call himself, fn recursive */
-	if (_isatty != 0)
-		main(1, argv2);
-	return (1);
-}
-/** Description: this fn allow print symbol or mesage before that user write*/
 void prompt(void)
 {
 	/** symbol > */
@@ -64,7 +10,11 @@ void prompt(void)
 	/** symbol space */
 	putchar(32);
 }
-/** Description; this fn allow get char the client terminal and call me fn until char is '\n'*/
+/**
+ * listenread - get char the client terminal & call fn until char is '\n'
+ *@buffer: pointer
+ * Return: 1 is successful, -1 if error.
+ */
 int listenread(char *buffer)
 {
 	size_t bufsize = SIZEBUFFER;
@@ -76,10 +26,12 @@ int listenread(char *buffer)
 		return (-1);
 	return (1);
 }
-
 /**
-* Description: this fn allow split the string when find the space char
-*/
+ * get_simple_args - this fn splita the string when space char is found
+ *@argc: argument counter
+ *@argv: where the arguments are contained
+ *@args: arguments
+ */
 void get_simple_args(int argc, char **argv __attribute__((unused)), char *args __attribute__((unused)))
 {
 	char delim[] = " \n";
@@ -108,11 +60,18 @@ void get_simple_args(int argc, char **argv __attribute__((unused)), char *args _
 	else if (argc == 2)
 		pid_launch(argv[1]);
 }
+/**
+ * _error - allows the error message to be printed in shell
+ *
+ */
 void _error(void)
 {
 	printf("/%s: No such file or directory\n", __FILE__);
 }
-
+/**
+ * pid_launch - allows shell to be launched
+ *@command: the command needed to pass to the machine to perform
+ */
 void pid_launch(char *command)
 {
 	pid_t pid;
@@ -123,7 +82,7 @@ void pid_launch(char *command)
 
 	if (strlen(command) > 0)
 	{
-		/** create id process (parent and chill) in when use to launch command*/
+		/** create id process (parent & child) to launch command*/
 		pid = fork();
 		if (pid == 0)
 			if (execve(command, _argv, envp) == -1)
