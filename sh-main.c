9a4815162207183fcae70a53adb64bbcd6b5c5b3
DaniVasq <danivasq50@gmail.com>
2019-04-16 03:29:10 +0000

    [

--- sh-main.c
+++ sh-main.c
@@ -5,6 +5,7 @@
   * main - Entry to launch program
   * @argc: argument counter
   * @argv: arguments
+  *@env: environment var
   * Return: 1.
   */
 int main(int argc, char **argv, char **env)
@@ -13,8 +14,10 @@ int main(int argc, char **argv, char **env)
 	char *line;
 	char *argv2[] = {__FILE__, NULL};
 	int _isatty;
-	char *_ARGS_PATH[] = {"/bin/","/sbin/","/usr/local/sbin/","/usr/local/bin/","/usr/sbin/","/usr/bin/","/snap/bin/", NULL};
-	
+	char *_ARGS_PATH[] = {"/bin/",
+"/sbin/", "/usr/local/sbin/", "/usr/local/bin/",
+"/usr/sbin/", "/usr/bin/", "/snap/bin/", NULL};
+
 	signal(SIGINT, intHandler);
 	_isatty = isatty(0);
 	if (argc == 1)
@@ -46,29 +49,40 @@ int main(int argc, char **argv, char **env)
 		main(1, argv2, env);
 	return (1);
 }
-
+/**
+ * intHandler - signal handler
+ *@i: variable integer
+ */
 void intHandler(int i __attribute__((unused)))
 {
 	fflush(stdout);
 }
-
+/**
+ * *getpath - gets the path
+ *@env: environment var pointer
+ * Return: space
+ */
 char *getpath(char **env)
-{	
+{
 	char *tmp;
 
 	while (*env != NULL)
 	{
 		tmp = strtok(*env, "=");
-		if (strcmp(tmp,"PATH") == 0)
+		if (strcmp(tmp, "PATH") == 0)
 		{
 			tmp = strtok(NULL, "\0");
-			return tmp;
+			return (tmp);
 		}
 		env++;
 	}
-	return "";
+	return ("");
 }
-
+/**
+ * **setpathparams - sets the path parameters
+ *@path: the path of the shell
+ * Return: params
+ */
 char **setpathparams(char *path)
 {
 	int i = 0;
@@ -77,7 +91,7 @@ char **setpathparams(char *path)
 	char **params;
 
 	params = malloc(sizeof(char *) * limitparams);
-	if(params == NULL)
+	if (params == NULL)
 		exit(114);
 	p = strtok(path, ":");
 	params[i] = p;
@@ -87,5 +101,5 @@ char **setpathparams(char *path)
 		p = strtok(NULL, ":");
 		params[i] = p;
 	}
-	return params;
+	return (params);
 }
